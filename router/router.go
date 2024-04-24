c21dec3c97f7c7a8e343817484bb7b9941285277
YouQian <1048474651@qq.com>
2024-04-24 11:55:21 +0800

    first commit

--- router/router.go
+++ router/router.go
@@ -4,11 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"management-backend/common"
 	"management-backend/conf"
+	"management-backend/controller"
 )
 
 func InitRouter(r *gin.Engine) {
 	v1 := r.Group("v1")
 	{
 		v1.GET("/", func(c *gin.Context) { common.ResOk(c, "Hello World!", conf.Mysql) })
+		user := v1.Group("user")
+		{
+			user.GET("/", controller.GetUserInfo)
+		}
 	}
 }
