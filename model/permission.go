877d10d655b69d074cbf14c85bafc0bbf825d331
YouQian <1048474651@qq.com>
2024-04-24 16:20:25 +0800

    2024-04-24 修改权限表字段

--- model/permission.go
+++ model/permission.go
@@ -2,9 +2,12 @@ package model
 
 type Permission struct {
 	Id        int           `json:"id" xorm:"pk autoincr INT(11)"`
-	Name      string        `json:"name" xorm:"varchar(64) not null"`
+	Uuid      int           `json:"uuid" xorm:"varchar(128) not null"`
+	Title     string        `json:"title" xorm:"varchar(64) not null"`
 	Path      string        `json:"path" xorm:"varchar(256) not null"`
 	Icon      string        `json:"icon" xorm:"varchar(512) not null"`
+	Component string        `json:"component" xorm:"varchar(128) not null"`
+	Desc      string        `json:"desc" xorm:"varchar(128) not null"`
 	Sort      int           `json:"sort" xorm:"int(11) not null default 100"`
 	Children  []*Permission `json:"children" xorm:"-"`
 	ParentId  int           `json:"parent_id" xorm:"int(11) not null default 0"`
