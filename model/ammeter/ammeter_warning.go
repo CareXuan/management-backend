196e5cf26d688d8662f6c29b85a090848f168210
YouQian <1048474651@qq.com>
2024-08-20 15:24:10 +0800

    2024-08-20 修改设备报警信息字段

--- model/ammeter/ammeter_warning.go
+++ model/ammeter/ammeter_warning.go
@@ -2,8 +2,18 @@ package ammeter
 
 type AmmeterWarning struct {
 	Id             int    `json:"id" xorm:"pk autoincr INT(11)"`
-	AmmeterId      int    `json:"ammeterId" xorm:"INT(11) not null"`
-	Type           int    `json:"type" xorm:"INT(11) not null default 0"`
+	AmmeterId      int    `json:"ammeter_id" xorm:"INT(11) not null"`
+	Type           string `json:"type" xorm:"varchar(128) not null default ''"`
+	AElectric      int    `json:"a_electric" xorm:"INT(11) not null default 0"`
+	BElectric      int    `json:"b_electric" xorm:"INT(11) not null default 0"`
+	CElectric      int    `json:"c_electric" xorm:"INT(11) not null default 0"`
+	AVoltage       int    `json:"a_voltage" xorm:"INT(11) not null default 0"`
+	BVoltage       int    `json:"b_voltage" xorm:"INT(11) not null default 0"`
+	CVoltage       int    `json:"c_voltage" xorm:"INT(11) not null default 0"`
+	Power          int    `json:"power" xorm:"INT(11) not null default 0"`
+	Leakage        int    `json:"leakage" xorm:"INT(11) not null default 0"`
+	Temperature    int    `json:"temperature" xorm:"INT(11) not null default 0"`
+	Times          int    `json:"times" xorm:"INT(11) not null default 0"`
 	Status         int    `json:"status" xorm:"INT(11) not null default 0"`
 	DealTime       int    `json:"-" xorm:"INT(11) not null default 0"`
 	DealTimeStr    string `json:"deal_time_str" xorm:"-"`
